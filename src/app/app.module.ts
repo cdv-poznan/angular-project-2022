24e6d79dc6768b92d73a374c2d1ae9423b20678d
paulinas24 <ps56015@gmail.com>
2022-05-14 12:53:15 +0200

    toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+import { ColorsComponent } from './colors/colors.component';
+import { ReminderComponent } from './reminder/reminder.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, ColorsComponent, ReminderComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
