af0d65fb0a6c79b0e0bb112bd08b4a4008befaca
Adrian Juszczak <adrian@juszczak.io>
2022-05-14 13:03:45 +0200

    Icons
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Angular Project 2022
+
+## Material
+
+https://material.angular.io
+
+## Icons
+
+https://fonts.google.com/icons
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import { HomeComponent } from './home/home.component';
-import { ColorsComponent } from './colors/colors.component';
-import { ReminderComponent } from './reminder/reminder.component';
+import {ColorsComponent} from './colors/colors.component';
+import {HomeComponent} from './home/home.component';
+import {ReminderComponent} from './reminder/reminder.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, ColorsComponent, ReminderComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
