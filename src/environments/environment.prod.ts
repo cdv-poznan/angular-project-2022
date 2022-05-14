8d0a2db83d3deaccd0bcfa664dea55a1b1527265
frontenddevmaciej <frontenddevmaciej@gmail.com>
2022-05-14 15:00:02 +0200

    Commit 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-project-2022-70cc1',
+    appId: '1:1094997464811:web:b252d2a41e35333688d0ae',
+    storageBucket: 'angular-project-2022-70cc1.appspot.com',
+    apiKey: 'AIzaSyBaHtBm6vst7d51r0XULxoaPr1wP_gLkl0',
+    authDomain: 'angular-project-2022-70cc1.firebaseapp.com',
+    messagingSenderId: '1094997464811',
+    measurementId: 'G-PWN35TM8CQ',
+  },
   production: true,
 };
