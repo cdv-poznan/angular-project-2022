030e0ebbf913342175190539d88436bfe55f6015
elachcdv <elach@edu.cdv.pl>
2022-05-14 12:55:51 +0200

    components, buttons toogle bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ColorsComponent} from './colors/colors.component';
+import {ReminderComponent} from './reminder/reminder.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'colors',
+    component: ColorsComponent,
+  },
+  {
+    path: 'reminder',
+    component: ReminderComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
