5db4f04fe9cb3d73359c138306d18af26e1e08e3
agrugru <a1gruszczynska@gmail.com>
2022-05-14 14:59:56 +0200

    t3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,35 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import { HomeComponent } from './home/home.component';
-import { RemindersComponent } from './reminders/reminders.component';
-import { ColorsComponent } from './colors/colors.component';
+import {HomeComponent} from './home/home.component';
+import {RemindersComponent} from './reminders/reminders.component';
+import {ColorsComponent} from './colors/colors.component';
+import {MatIconModule} from '@angular/material/icon';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment';
+import {provideAuth, getAuth} from '@angular/fire/auth';
+import {provideFirestore, getFirestore} from '@angular/fire/firestore';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, RemindersComponent, ColorsComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angular-project2022',
+    appId: '1:861764576351:web:feddcb7fdf8142526267ea',
+    storageBucket: 'angular-project2022.appspot.com',
+    apiKey: 'AIzaSyDmyoUf3jEGRmFfPZggVU2vaD68MFApKhw',
+    authDomain: 'angular-project2022.firebaseapp.com',
+    messagingSenderId: '861764576351',
+    measurementId: 'G-VZ47MX81WC',
+  },
   production: false,
 };
 
