cabf191aa157d4fa28a32c327f6c623e36b3c462
JZujewski1 <juliusz.zujewski@gmail.com>
2022-05-14 12:14:42 +0200

    Buttons components, routerLink to buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import {AppComponent} from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import { ReminderComponent } from './reminder/reminder.component';
+import { ColorsComponent } from './colors/colors.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ReminderComponent, ColorsComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
