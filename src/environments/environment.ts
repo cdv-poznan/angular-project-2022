2ae995aa805ff20e0fd446c1fb5d206de9759601
Dan199312 <dan199312@gmail.com>
2022-05-14 13:55:26 +0200

    firebase add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angular-project-2022-84ff2',
+    appId: '1:895368187520:web:d3d25e9755da44c19f2065',
+    storageBucket: 'angular-project-2022-84ff2.appspot.com',
+    apiKey: 'AIzaSyAAt6tisal2FL9JMYe1DZINkubHBI84yK4',
+    authDomain: 'angular-project-2022-84ff2.firebaseapp.com',
+    messagingSenderId: '895368187520',
+    measurementId: 'G-5BCTYPNF62',
+  },
   production: false,
 };
 
