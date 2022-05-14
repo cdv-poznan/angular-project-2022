f913e61f6c0a4724d6a1ad79c0290ee8104c1353
Gerwix <mateuszgerwazik@gmail.com>
2022-05-14 13:55:30 +0200

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-cdv-2022',
+    appId: '1:784786840701:web:bb2d312f0d91f7374c2514',
+    storageBucket: 'angular-cdv-2022.appspot.com',
+    apiKey: 'AIzaSyD-nbsIZOqN1Sk7rpMXGLyxAENXOyCb5n8',
+    authDomain: 'angular-cdv-2022.firebaseapp.com',
+    messagingSenderId: '784786840701',
+    measurementId: 'G-EXDL6Y7LCS',
+  },
   production: true,
 };
