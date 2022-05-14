6c97b8a89cfa1367614aaa20f15fcf2b760abb61
Adrian Juszczak <adrian@juszczak.io>
2022-05-14 12:46:21 +0200

    Routing
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+import { ColorsComponent } from './colors/colors.component';
+import { ReminderComponent } from './reminder/reminder.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, ColorsComponent, ReminderComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
