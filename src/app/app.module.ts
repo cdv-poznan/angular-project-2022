992725556b879101f4ebbafd325d4e779be2fa13
agrugru <a1gruszczynska@gmail.com>
2022-05-15 10:45:11 +0200

    Colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
@@ -15,18 +16,26 @@ import {provideAuth, getAuth} from '@angular/fire/auth';
 import {provideFirestore, getFirestore} from '@angular/fire/firestore';
 import {MatCardModule} from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, RemindersComponent, ColorsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
     MatListModule,
+    MatTooltipModule,
+    ClipboardModule,
+    MatSnackBarModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
--- src/app/colors/colors.component.ts
+++ src/app/colors/colors.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Color} from './model/color';
+import {ColorResponse} from './model/color-response';
 
 @Component({
   selector: 'app-colors',
   templateUrl: './colors.component.html',
-  styleUrls: ['./colors.component.scss']
+  styleUrls: ['./colors.component.scss'],
 })
 export class ColorsComponent implements OnInit {
-
-  constructor() { }
+  colors: Color[];
+  constructor(private httpClient: HttpClient, private matSnackBar: MatSnackBar) {}
 
   ngOnInit(): void {
+    this.httpClient.get<ColorResponse>('https://reqres.in/api/colors').subscribe((response: ColorResponse) => {
+      this.colors = response.data;
+    });
   }
-
+  onCopied() {
+    this.matSnackBar.open('Color has been copied to clipboard', 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
+      duration: 6000,
+    });
+  }
 }
