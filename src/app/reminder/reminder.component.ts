f851b301138b99b306a644837e4617486caef51c
Adrian Juszczak <adrian@juszczak.io>
2022-05-15 14:47:21 +0200

    Convert Data
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/app/reminder/reminder.component.ts
+++ src/app/reminder/reminder.component.ts
@@ -1,5 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {collection, collectionData, DocumentData, Firestore} from '@angular/fire/firestore';
+import {collection, collectionData, DocumentData, Firestore, QueryDocumentSnapshot} from '@angular/fire/firestore';
+
+interface Task {
+  text: string;
+  created: number;
+  done: boolean;
+}
 
 @Component({
   selector: 'app-reminder',
@@ -7,12 +13,19 @@ import {collection, collectionData, DocumentData, Firestore} from '@angular/fire
   styleUrls: ['./reminder.component.scss'],
 })
 export class ReminderComponent implements OnInit {
-  tasks: DocumentData[];
+  tasks: Task[];
 
   constructor(public firestore: Firestore) {}
 
   ngOnInit(): void {
-    const remindersCollection = collection(this.firestore, 'reminders');
+    const remindersCollection = collection(this.firestore, 'reminders').withConverter({
+      toFirestore(task: Task): DocumentData {
+        return task as DocumentData;
+      },
+      fromFirestore(snapshot: QueryDocumentSnapshot): Task {
+        return snapshot.data() as Task;
+      },
+    });
 
     collectionData(remindersCollection).subscribe(data => {
       this.tasks = data;
