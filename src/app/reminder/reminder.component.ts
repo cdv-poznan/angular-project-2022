ff104d25ad51e1fed84e9e75dc61a7b942bf014a
martabakaj <marta.bakaj@gmail.com>
2022-05-15 14:37:35 +0200

    wyświetlanie z bazy danych basa data

--- src/app/reminder/reminder.component.ts
+++ src/app/reminder/reminder.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {collection, collectionData, DocumentData, Firestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-reminder',
   templateUrl: './reminder.component.html',
-  styleUrls: ['./reminder.component.scss']
+  styleUrls: ['./reminder.component.scss'],
 })
 export class ReminderComponent implements OnInit {
+  tasks: DocumentData[];
 
-  constructor() { }
+  constructor(public firestore: Firestore) {}
 
   ngOnInit(): void {
+    const remindersCollection = collection(this.firestore, 'reminders');
+    collectionData(remindersCollection).subscribe(data => {
+      this.tasks = data;
+    });
   }
-
 }
