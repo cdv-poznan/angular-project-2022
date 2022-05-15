a0283697cc422753edc65955c3058d089005bb0f
kasiasolinska <catherine.bus@gmail.com>
2022-05-15 14:49:17 +0200

    konwerter

--- src/app/reminder/reminder.component.ts
+++ src/app/reminder/reminder.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { collectionData, Firestore } from '@angular/fire/firestore';
-import { collection, DocumentData } from '@firebase/firestore';
+import { collection, DocumentData, QueryDocumentSnapshot } from '@firebase/firestore';
+
+
+interface Task {
+  text: string;
+  created: number;
+  done: boolean;
+}
 
 @Component({
   selector: 'app-reminder',
@@ -8,12 +15,20 @@ import { collection, DocumentData } from '@firebase/firestore';
   styleUrls: ['./reminder.component.scss']
 })
 export class ReminderComponent implements OnInit {
-  tasks: DocumentData[];
+  tasks: Task[];
 
   constructor(public firestore:Firestore) { }
 
   ngOnInit(): void {
-    const remindersCollection = collection(this.firestore, 'reminders');
+    const remindersCollection = collection(this.firestore, 'reminders').withConverter({
+      toFirestore(task: Task): DocumentData{
+       return task as DocumentData;
+      },
+      fromFirestore(snapshot: QueryDocumentSnapshot): Task {
+        return snapshot.data() as Task;
+      }
+    });
+
 
     collectionData(remindersCollection).subscribe(data => {
       this.tasks = data;
