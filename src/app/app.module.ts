070584de67cfbc743cd977f6e495d6bd5ab2bdc9
elachcdv <elach@edu.cdv.pl>
2022-05-15 10:51:49 +0200

    color moduls - list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
+// angular
 import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+// angular materials
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {ColorsComponent} from './colors/colors.component';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+// custom components
+import {HomeComponent} from './home/home.component';
+import {ReminderComponent} from './reminder/reminder.component';
+// firebase services and enviorment module
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {environment} from '../environments/environment';
 import {provideAuth, getAuth} from '@angular/fire/auth';
 import {provideFirestore, getFirestore} from '@angular/fire/firestore';
-import {MatCardModule} from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
-import {ColorsComponent} from './colors/colors.component';
-import {HomeComponent} from './home/home.component';
-import {ReminderComponent} from './reminder/reminder.component';
 
 @NgModule({
   declarations: [AppComponent, ColorsComponent, HomeComponent, ReminderComponent],
@@ -22,11 +30,15 @@ import {ReminderComponent} from './reminder/reminder.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
     MatListModule,
+    MatTooltipModule,
+    ClipboardModule,
+    MatSnackBarModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
--- src/app/colors/colors.component.ts
+++ src/app/colors/colors.component.ts
@@ -1,4 +1,8 @@
+import {HttpClient} from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Color} from './model/color';
+import {ColorResponse} from './model/color-response';
 
 @Component({
   selector: 'app-colors',
@@ -6,7 +10,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./colors.component.scss'],
 })
 export class ColorsComponent implements OnInit {
-  constructor() {}
+  // tutaj deklaracja jakiego typu modelem jest colors (tablica Color)
+  colors: Color[];
+  snackbar: any;
 
-  ngOnInit(): void {}
+  constructor(private httpClient: HttpClient, private matSnackBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+    this.httpClient.get<ColorResponse>('https://reqres.in/api/colors').subscribe((response: ColorResponse) => {
+      this.colors = response.data; //odwołanie do tego jaką odpowiedź daje colors
+    });
+  }
+
+  onCopiedToClipboard() {
+    this.matSnackBar.open('Color copied to clipboard!', 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
+      duration: 3000,
+    });
+  }
 }
