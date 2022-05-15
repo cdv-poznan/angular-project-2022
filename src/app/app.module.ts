8bd41d82b9440ed01c48a41fd76ee9d62294093b
paulinas24 <ps56015@gmail.com>
2022-05-15 12:09:56 +0200

    translator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,49 @@
+import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {environment} from '../environments/environment';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import { HomeComponent } from './home/home.component';
-import { ColorsComponent } from './colors/colors.component';
-import { ReminderComponent } from './reminder/reminder.component';
-import {MatIconModule} from '@angular/material/icon';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import {MatCardModule} from '@angular/material/card';
-import { HttpClientModule } from '@angular/common/http';
-import {MatListModule} from '@angular/material/list';
-
+import {ColorsComponent} from './colors/colors.component';
+import {HomeComponent} from './home/home.component';
+import {ReminderComponent} from './reminder/reminder.component';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {TranslateModule} from '@ngx-translate/core';
 
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, ColorsComponent, ReminderComponent],
-  imports: [BrowserModule, MatListModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule, MatCardModule, MatIconModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
+  imports: [
+    BrowserModule,
+    TranslateModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    ClipboardModule,
+    MatSnackBarModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/colors/colors.component.ts
+++ src/app/colors/colors.component.ts
@@ -1,7 +1,9 @@
 import {HttpClient} from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
-import { Color } from './model/color';
-import { ColorsResponse } from './model/colors-response';
+import {PageEvent} from '@angular/material/paginator';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Color} from './model/color';
+import {ColorsResponse} from './model/colors-response';
 
 @Component({
   selector: 'app-colors',
@@ -10,13 +12,35 @@ import { ColorsResponse } from './model/colors-response';
 })
 export class ColorsComponent implements OnInit {
   colors: Color[];
+  total: number = 0;
+  perPage: number = 0;
 
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient, private matSnackBar: MatSnackBar) {}
 
   ngOnInit(): void {
-    this.httpClient.get<ColorsResponse>('https://reqres.in/api/colors')
-      .subscribe((response: ColorsResponse)=> {
-  this.colors = response.data;      });
+    this.getColors(1);
+  }
+
+  getColors(page:number){
+    this.httpClient.get<ColorsResponse>('https://reqres.in/api/colors', {
+      params: {page},
+    }).subscribe((response: ColorsResponse) => {
+      this.colors = response.data;
+      this.total = response.total;
+      this.perPage = response.per_page;
+    });
+  }
+
+  onCopied() {
+    this.matSnackBar.open('Color has been copied to clipboard', 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
+      duration: 2000,
+    });
+  }
+
+  onPageChange(event: PageEvent) {
+    const nextPage = event.pageIndex + 1;
+   this.getColors(nextPage);
   }
 }
