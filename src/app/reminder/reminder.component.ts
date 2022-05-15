cf9b333ff5d54b60646b943c1c7c633585f4ed00
elachcdv <elach@edu.cdv.pl>
2022-05-15 14:52:30 +0200

    firebase data converter

--- src/app/reminder/reminder.component.ts
+++ src/app/reminder/reminder.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {collectionData, Firestore} from '@angular/fire/firestore';
+import {collection, DocumentData, QueryDocumentSnapshot} from '@firebase/firestore';
+
+interface Task {
+  text: string;
+  created: number;
+  done: boolean;
+}
 
 @Component({
   selector: 'app-reminder',
   templateUrl: './reminder.component.html',
-  styleUrls: ['./reminder.component.scss']
+  styleUrls: ['./reminder.component.scss'],
 })
 export class ReminderComponent implements OnInit {
+  tasks: Task[];
 
-  constructor() { }
+  constructor(public firestore: Firestore) {}
 
   ngOnInit(): void {
+    //referencja że chcemy pobrać dane z bazy danych - z kolekcji
+    const remindersCollection = collection(this.firestore, 'reminders').withConverter({
+      toFirestore(task: Task): DocumentData {
+        return task as DocumentData;
+      },
+      fromFirestore(snapshot: QueryDocumentSnapshot): Task {
+        return snapshot.data() as Task;
+      },
+    });
+    collectionData(remindersCollection).subscribe(data => {
+      this.tasks = data;
+    });
   }
-
 }
