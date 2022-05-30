6789b5d77878917934c1807c578ff9bd129f23ba
PeterM8811 <pmlynarski88@gmail.com>
2022-05-30 19:25:34 +0200

    new title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { ListOfResultsComponent } from './list-of-results/list-of-results.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ListOfResultsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
