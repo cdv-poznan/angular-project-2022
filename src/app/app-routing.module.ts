cd06e87698f1e63d52813be2c8cad72125f2141a
PeterM8811 <pmlynarski88@gmail.com>
2022-05-30 19:57:43 +0200

    background

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import { resourceLimits } from 'worker_threads';
 import { ListOfResultsComponent } from './list-of-results/list-of-results.component';
 
 const routes: Routes = [
 {
   path: '',
-  component: list-of-ListOfResultsComponent,
+  component: ListOfResultsComponent,
 }
 ];
 
