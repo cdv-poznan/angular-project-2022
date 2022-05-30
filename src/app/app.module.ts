32f8730473b023b522b964b64d0463d34e425621
PeterM8811 <pmlynarski88@gmail.com>
2022-05-30 20:26:36 +0200

    module ICON, Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { ListOfResultsComponent } from './list-of-results/list-of-results.component';
+import {ListOfResultsComponent} from './list-of-results/list-of-results.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, ListOfResultsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [MatButtonModule, MatIconModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
