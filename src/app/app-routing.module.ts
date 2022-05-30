7133a3fcc59e8042c7b2e5cbd5d08b0be41e10a6
PeterM8811 <pmlynarski88@gmail.com>
2022-05-30 19:33:40 +0200

    new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import { resourceLimits } from 'worker_threads';
+import { ListOfResultsComponent } from './list-of-results/list-of-results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '',
+  component: list-of-ListOfResultsComponent,
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
