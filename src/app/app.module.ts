020b994e1a7b7f6fe97abfc4423c9cd8dd0d33b2
MiloszStochaj <milosz.stochaj@gmail.com>
2022-06-09 17:31:05 +0200

    content tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,38 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { TableComponent } from './table/table.component';
+import {TableComponent} from './table/table.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {MatTabsModule} from '@angular/material/tabs';
 
 
 @NgModule({
   declarations: [AppComponent, TableComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule, MatPaginatorModule, MatFormFieldModule, FontAwesomeModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    MatTabsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
