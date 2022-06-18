6550f047b59e5a57011e4b07da097ee8af90dc66
paulinas24 <ps56015@gmail.com>
2022-06-18 10:29:45 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MoreinfosComponent } from './component/moreinfos/moreinfos.component';
+import { UsersfileComponent } from './component/usersfile/usersfile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users', component: UsersfileComponent},
+  {path: 'MoreInfo/:uuid', component: MoreinfosComponent},
+  {path: '**', redirectTo: 'users'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersfileComponent } from './component/usersfile/usersfile.component';
+import { MoreinfosComponent } from './component/moreinfos/moreinfos.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    UsersfileComponent,
+    MoreinfosComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
