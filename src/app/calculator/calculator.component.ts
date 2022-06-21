078e5a6cb5efc19b2a3743f447ea1b9023fd18d2
MiloszStochaj <milosz.stochaj@gmail.com>
2022-06-21 14:59:31 +0200

    calculator update

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,24 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 
 
+interface Result {
+  profit: number;
+  loss: number;
+  reqDep: number;
+}
+
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
+
+
+
 export class CalculatorComponent implements OnInit {
+results: Result[] = [];
 public price: number;
 public quantity: number;
-public profit: number;
-public stop: number;
+public takeProfit: number;
+public stopLoss: number;
 public laverage: number;
+
+
 
   constructor() { }
 
+
+
  public getValue(){
    let laverage = Number(this.laverage)
-   console.log(this.price, this.quantity, laverage, this.profit, this.stop)
+   let profit = (this.quantity * this.takeProfit) - (this.quantity * this.price)
+   let loss = (this.quantity * this.stopLoss) - (this.quantity * this.price)
+   let worth = this.price * this.quantity
+   let requiredDeposit = worth/laverage
+   console.log(profit, loss, requiredDeposit)
+
+   const result: Result = {
+    profit: profit,
+    loss: loss,
+    reqDep: requiredDeposit,
+
+  }
+  console.log(result)
+  this.results.push(result)
+ 
  }
+
+
 
   ngOnInit(): void {
   }
