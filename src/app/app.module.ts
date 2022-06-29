c8f376e6e180ee7fea0808d4ad9cf5d148d4bc8d
paulinas24 <ps56015@gmail.com>
2022-06-29 18:44:13 +0200

    navbar and notes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,14 @@ import { UsersfileComponent } from './component/usersfile/usersfile.component';
 import { MoreinfosComponent } from './component/moreinfos/moreinfos.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
+import { NavbarMainComponent } from './navbar-main/navbar-main.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { NotesComponent } from './component/notes/notes.component';
 
 
 @NgModule({
@@ -14,13 +22,15 @@ import {MatCardModule} from '@angular/material/card';
     AppComponent,
     UsersfileComponent,
     MoreinfosComponent,
+    NavbarMainComponent,
+    NotesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule, 
-    MatCardModule
+    MatCardModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar-main/navbar-main.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navbar-main',
+  templateUrl: './navbar-main.component.html',
+  styleUrls: ['./navbar-main.component.css']
+})
+export class NavbarMainComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
